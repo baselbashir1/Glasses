439e5199170111a031c1d8a5a0e07871568c9ed8
baselbashir1 <eng.baselbashir@gmail.com>
2023-08-04 12:47:41 +0300

    fix logout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -62,7 +62,7 @@
             </li>
 
             <li class="menu">
-                <a href="./app-calendar.html" aria-expanded="false" class="dropdown-toggle">
+                <a href="/dossiers" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -74,7 +74,25 @@
                             <line x1="8" y1="2" x2="8" y2="6"></line>
                             <line x1="3" y1="10" x2="21" y2="10"></line>
                         </svg>
-                        <span>Calendar</span>
+                        <span>Dossiers</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="menu">
+                <a href="/agents" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-calendar">
+                            <rect x="3" y="4" width="18" height="18" rx="2"
+                                ry="2">
+                            </rect>
+                            <line x1="16" y1="2" x2="16" y2="6"></line>
+                            <line x1="8" y1="2" x2="8" y2="6"></line>
+                            <line x1="3" y1="10" x2="21" y2="10"></line>
+                        </svg>
+                        <span>Agents</span>
                     </div>
                 </a>
             </li>
@@ -88,7 +106,7 @@
                             <line x1="12" y1="1" x2="12" y2="23"></line>
                             <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
                         </svg>
-                        <span>Invoice</span>
+                        <span>Invoices</span>
                     </div>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
